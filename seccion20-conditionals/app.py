454c5bbd383f08fce6ba67578acc2dbd30084d46
facundovillalba38 <facundovillalba38@gmail.com>
2020-11-22 20:13:44 -0300

    conditionals

---
+++ seccion20-conditionals/app.py
@@ -0,0 +1,20 @@
+# Booleans
+#    true or False
+
+# Break, Continue, Pass
+
+for i in range(1,11):
+     if i==8:
+          break
+     elif i==5:
+          continue
+     else i==6:
+          pass
+     print(i)
+
+
+"""
+Break - stops the whole loop
+Continue - Stop current iteration of the loop
+Pass - passes over the current statement and continues the loop as normal
+"""
